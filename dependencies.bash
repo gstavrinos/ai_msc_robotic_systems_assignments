a9bbd8ca1056e8ace9e295a6384d68540fc30354
George Stavrinos <gstavrinos@protonmail.com>
2023-05-06 18:36:46 +0300

    final (?) fixes

--- dependencies.bash
+++ dependencies.bash
@@ -1,9 +1,10 @@
 #!/bin/bash
+curr_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ws_dir=$curr_dir/../..
 sudo apt update
 sudo apt install -y python3-rosinstall
-rosinstall src/ ai_msc.rosinstall
-ws_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../..
-rm -rf src/gazebo_ros2_control
-mv src/ros2_controllers/diff_drive_controller src/
-rm -rf src/ros2_controllers/*
-mv src/diff_drive_controller src/ros2_controllers/
+rosinstall $ws_dir/src/ $curr_dir/ai_msc.rosinstall
+rm -rf $ws_dir/src/gazebo_ros2_control
+mv $ws_dir/src/ros2_controllers/diff_drive_controller $ws_dir/src/
+rm -rf $ws_dir/src/ros2_controllers/*
+mv $ws_dir/src/diff_drive_controller $ws_dir/src/ros2_controllers/
