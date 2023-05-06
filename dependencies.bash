95cc700d623b4cfc8dcc495b3e47d1a117f3689c
George Stavrinos <gstavrinos@protonmail.com>
2023-05-06 18:15:11 +0300

    Improved automation script with directory discovery

--- dependencies.bash
+++ dependencies.bash
@@ -1,6 +1,8 @@
+#!/bin/bash
 sudo apt update
 sudo apt install -y python3-rosinstall
 rosinstall src/ ai_msc.rosinstall
+ws_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../..
 rm -rf src/gazebo_ros2_control
 mv src/ros2_controllers/diff_drive_controller src/
 rm -rf src/ros2_controllers/*
